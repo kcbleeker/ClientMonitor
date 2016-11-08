354739746a07ed33e0bd2016193240b6101ce53d
Andrew <bluntspoon@gmail.com>
2016-11-08 23:47:26 +0200

    tweaks to ui and logging

--- scripts/uptime.js
+++ scripts/uptime.js
@@ -7,6 +7,7 @@ var options = {
 $(document).ready(function () {
     setupRouterMonitor();
     setupLobbyMonitor();
+    setupLogging();
 });
 
 function setupRouterMonitor() {
@@ -14,8 +15,10 @@ function setupRouterMonitor() {
     //var url = "https://pd4router.installprogram.eu/pokerBabelFish";
     var url = "https://mppgs1mobile.valueactive.eu/pokerBabelFish";
     var routerConnection = $.connection(url);
+    routerConnection.logging = true;
     routerConnection.start();
     $("#routerUpdateTime").text("Starting...");
+    $("#routerUrl").text(url);
     var routerTimer = setTimeout(function () { $("#router").removeClass("up"); }, routerTimeoutMilliSec);
     routerConnection.received(function (data) {
         clearTimeout(routerTimer);
@@ -31,6 +34,12 @@ function setupRouterMonitor() {
             routerConnection.start();
         }, 5000); // Restart connection after 5 seconds.
     });
+
+    routerConnection.error(function (error) {
+        $("#router").removeClass("up");
+        $("#routerUpdateTime").text("ERROR");
+        console.log('Router SignalR error: ' + error)
+    });
 }
 
 function setupLobbyMonitor() {
@@ -38,8 +47,10 @@ function setupLobbyMonitor() {
     //var url = "https://pd4lobby.installprogram.eu/pokerBabelFish"
     var url = "https://mpplobby3mobile.valueactive.eu/pokerBabelFish";
     var lobbyConnection = $.connection(url);
+    lobbyConnection.logging = true;
     lobbyConnection.start();
     $("#lobbyUpdateTime").text("Starting...");
+    $("#lobbyUrl").text(url);
     var lobbyTimer = setTimeout(function () { $("#lobby").removeClass("up"); }, lobbyTimeoutMilliSec);
 
     lobbyConnection.received(function (data) {
@@ -56,4 +67,24 @@ function setupLobbyMonitor() {
             lobbyConnection.start();
         }, 5000); // Restart connection after 5 seconds.
     });
+
+    lobbyConnection.error(function (error) {
+        $("#lobby").removeClass("up");
+        $("#lobbyUpdateTime").text("ERROR");
+        console.log('Lobby SignalR error: ' + error)
+    });
+}
+
+function setupLogging() {
+    if (typeof console != "undefined")
+        if (typeof console.log != 'undefined')
+            console.olog = console.log;
+        else
+            console.olog = function () { };
+
+    console.log = function (message) {
+        console.olog(message);
+        $('#log').append('<p>' + message + '</p>');
+    };
+    console.error = console.debug = console.info = console.log
 }
